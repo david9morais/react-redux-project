298e57a60b09460efacff6cab2991cd0b6598c43
david9morais <david_morais@hotmail.com>
2021-03-28 23:49:03 +0100

    change components and install redux

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
 import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
 import './App.css';
-import List from './components/List';
-import User from './components/User';
-import photo from './img/user.png'
+import routesConfig from './routesConfig';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: "David",
-      email: "david@gmail.com"
-    }
 
-    this.changeState = this.changeState.bind(this)
-    this.resetState = this.resetState.bind(this)
-    this.changeInput = this.changeInput.bind(this)
-  }
-
-  changeState() {
-    this.setState({
-      name: "David Morais"
-    })
-  }
-
-  resetState() {
-    this.setState({
-      name: "David"
-    })
-  }
-
-  changeInput(event) {
-    let target = event.target
-    let index = target.name
-    this.setState({
-      [index]: target.value
-    })
-  }
 
   render() {
     return (
-      <div className="App">
-        <div>
-          <form>
-            <label>Nome
-              <input type="text" name="name" value={this.state.name} onChange={this.changeInput}></input>
-            </label>
-            <label>Email
-              <input type="text" name="email" value={this.state.email} onChange={this.changeInput}></input>
-            </label>
-          </form>
-          <User name={this.state.name} email={this.state.email} photo={photo} />
+      <div>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/user">User</Link>
         </div>
-        <button onClick={this.changeState}>Change Name</button>
-        <button onClick={this.resetState}>Reset Name</button>
-        <List />
+        {routesConfig.map((value, key) => {
+          return <Route key={key} path={value.path} component={value.component} exact={value.exact} ></Route>
+        })}
       </div>
     );
   }
--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class List extends Component {
+class User extends Component {
     render() {
         let list = [
             {
@@ -38,4 +38,4 @@ class List extends Component {
     }
 }
 
-export default List
+export default User
